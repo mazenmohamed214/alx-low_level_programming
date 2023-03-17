135b3a7ade6369a96cc6a66c5f342b080cf82cbe
skinchizer <mezo214@gmail.com>
2023-03-17 05:30:32 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
