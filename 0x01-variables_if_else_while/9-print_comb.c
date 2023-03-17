1932f70ddfdc63155dc4bc9146be816afdd5f59b
skinchizer <mezo214@gmail.com>
2023-03-17 05:51:40 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:Always 0
+ */
+int main(void)
+{
+	int i =0;
+
+	for (i = 48 ; i < 58 ; i++ )
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
