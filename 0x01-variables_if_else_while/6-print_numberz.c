2020304be41a03d0c286887459c0a1a4d4a71a0b
skinchizer <mezo214@gmail.com>
2023-03-17 05:38:38 -0700

    s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48 ; i < 58 ; i++);
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
