da332b9d448f5de1984c6ef5b292c2a08775b5f4
skinchizer <mezo214@gmail.com>
2023-03-17 05:17:57 -0700

    s

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Retrun: always 0
+ */
+int main(void)
+{
+	int i = 0;
+	for(i = 97;i < 123;i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
