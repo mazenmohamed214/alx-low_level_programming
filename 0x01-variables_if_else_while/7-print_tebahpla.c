9e7a5b6304d01c8f9dec2bc395de8ed618a73e18
skinchizer <mezo214@gmail.com>
2023-03-17 05:41:38 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 122 ; i > 96 ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
