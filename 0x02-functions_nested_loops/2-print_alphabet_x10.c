24ff3a6802b27a5785b6ce35ec69bb60287a5f58
skinchizer <mezo214@gmail.com>
2023-03-21 05:55:34 -0700

    s

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * print alphabet x10 - prints alphabet in lowercase times ten
+ * print_alphabet_x10 - prints alphabet in lowercase times ten
  */
 
 void print_alphabet_x10(void)
 {
 	int counter;
 
-	for (counter = 0 ; counter < 9 ; counter++)
+	for (counter = 0 ; counter < 10 ; counter++)
 	{
 		char letter;
+
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
+
 		_putchar('\n');
 	}
 }
