f32ffdf5df2addff54acc217412cddd09af9fa3a
skinchizer <mezo214@gmail.com>
2023-04-10 18:46:58 -0700

    s

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ * @argc: program arguments
+ * @argv: array of argc
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
