73278455cf3bfc063f6e6e4006922fca49459772
skinchizer <mezo214@gmail.com>
2023-04-11 18:29:39 -0700

    s

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 
 	if (size == 0)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 	arr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
