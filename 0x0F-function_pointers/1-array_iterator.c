68689ece3e3fa6b140a6f68656feb44be84177f0
skinchizer <mezo214@gmail.com>
2023-04-19 08:48:43 -0700

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array && action && size > 0)
 	{
 		unsigned int i;
+
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
