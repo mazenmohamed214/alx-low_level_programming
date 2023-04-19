90c2faa735184a07ad983a7f6b846e197e6ae825
skinchizer <mezo214@gmail.com>
2023-04-19 08:29:24 -0700

    s

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: pointer to different functions
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+		f(name);
+}
