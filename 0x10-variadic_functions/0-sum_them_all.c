85d3db920514318752f60e5bed9c0d766cee9f6e
skinchizer <mezo214@gmail.com>
2023-04-20 08:39:57 -0700

    s

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - adds sum of variable nums
+ * @n: no of args
+ * Return: integer of sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+}
