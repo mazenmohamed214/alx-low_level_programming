03ae0d374d437d6f4e1f7ebbeae7ce904906e82d
skinchizer <mezo214@gmail.com>
2023-04-20 08:44:14 -0700

    s

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,12 +7,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
 	if (n == 0)
 		return (0);
-	va_list list;
-
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
