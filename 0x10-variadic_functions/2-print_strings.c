198f45afa5351613fdba9c70b7935d31d16e59e5
skinchizer <mezo214@gmail.com>
2023-04-21 00:17:10 -0700

    s

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,8 +13,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(list, char *))
-			printf("%s", va_arg(list, char *));
+		char *m = va_arg(list, char *);
+		if (m)
+			printf("%s", m);
 		else
 			printf("(nil)");
 		if (separator && i != n - 1)
