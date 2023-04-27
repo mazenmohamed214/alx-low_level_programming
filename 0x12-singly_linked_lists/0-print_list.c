99a0db97d7e3309e96f381c3bed525c5837c0ee8
skinchizer <mezo214@gmail.com>
2023-04-27 11:49:48 -0700

    s

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		n = n->next;
+		h = h->next;
 		size++;
 	}
 	return (size);
