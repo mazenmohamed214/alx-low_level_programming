3c71fe23d27f2a2cde149b0ac2b67f6cb79eb249
skinchizer <mezo214@gmail.com>
2023-05-03 05:26:49 -0700

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,11 +7,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	size_t count = 0;
+
 	if (h == NULL)
 		return (1);
 	while (h->next)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
+	return (count);
 }
