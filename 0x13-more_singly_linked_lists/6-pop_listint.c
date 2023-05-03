a111c183a0d5d1af4ea9281d12e490687040b369
skinchizer <mezo214@gmail.com>
2023-05-03 08:04:46 -0700

    s

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - pop a node
+ * @head: dbl ptr to list
+ *
+ * Return: node poped
+ */
+int pop_listint(listint_t **head)
+{
+	int poped;
+
+	if (*head || head)
+		return (0);
+	poped = (*head)->n;
+	(*head) = (*head)->next;
+	return (poped);
+}
