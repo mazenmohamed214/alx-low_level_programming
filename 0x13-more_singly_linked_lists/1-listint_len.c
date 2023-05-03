855dfe12c2b89319938f64f7cb1ab0e0b2b3aa34
skinchizer <mezo214@gmail.com>
2023-05-03 05:39:44 -0700

    s

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len - prints length of linked list
+ * @h: linked list
+ *
+ * Return: no of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
