12d0e3d133db2dc5a622ae4b88b2fdbd750c0e69
skinchizer <mezo214@gmail.com>
2023-05-03 07:39:49 -0700

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,6 @@
  *
  * Return: no of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - add node end
+ * @head: double ptr do linked list
+ * @n: node data
+ *
+ * Return: address of new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *tmp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
