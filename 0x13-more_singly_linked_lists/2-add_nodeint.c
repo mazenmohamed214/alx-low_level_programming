e8b47f57a2bb211c81bf6ef555beb1285522f84d
skinchizer <mezo214@gmail.com>
2023-05-03 07:21:34 -0700

    s

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint - adds node from start
+ * @head: double ptr to linked list
+ * @n: data of node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (1);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
